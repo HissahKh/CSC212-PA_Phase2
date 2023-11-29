55b47cc0e1f9ca5b4d0c28951d9815a897accab2
11h2v0 <146987469+11h2v0@users.noreply.github.com>
2023-11-29 18:05:20 +0300

    Update Node.java

--- Node.java
+++ Node.java
@@ -1,47 +1,36 @@
-
 public class Node<T>{
 	
 	public T data;
     public Node<T> next;
 		
 		
-	public Node(){
-			
-		data = null;
-		next = null;
-			
-	}
+		public Node() {
+			data = null;
+			next = null;
+		}
 		
-	public Node(T data){
+		public Node(T data) {
 			
-		this.data = data;
-		next = null;
+			this.data = data;
+			next = null;
 			
-	}
+		}
 
 
-	public T getData(){
-	    	
-		return data;
-			
-	}
+	        public T getData() {
+			return data;
+		}
 
-	public void setData(T data){
-			
-		this.data = data;
-			
-	}
+		public void setData(T data) {
+			this.data = data;
+		}
 
-	public Node<T> getNext(){
-			
-		return next;
-			
-	}
+		public Node<T> getNext() {
+			return next;
+		}
 
-	public void setNext(Node<T> next){
-			
-		this.next = next;
-		
-	}
+		public void setNext(Node<T> next) {
+			this.next = next;
+		}
 		
 }
